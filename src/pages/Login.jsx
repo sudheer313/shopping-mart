06a310d20f64d38b9c4316f39e003e035ddca658
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-11-30 20:20:42 +1100

    Google Authentication setup using firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { auth, googleProvider } from "../firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+//useNavigateHook
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result.user);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSignout = async () => {
+    try {
+      const result = await signOut(auth);
+      console.log("signout sucessfull");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}>login</button>
+      <button onClick={handleSignout}>Signout</button>
+    </div>
+  );
 };
 
 export default Login;
